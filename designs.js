d7efd81f6321c057bda5ac2fbdbb3ba773ed8a64
Kassandra Saint <kassandrastripper@gmail.com>
2022-04-23 11:14:43 -0400

    Refuctored to reduce repitition. Cleared table for next submit.

--- designs.js
+++ designs.js
@@ -1,20 +1,27 @@
 // Getting elements by id for form and table elements
-var form = document.getElementById("sizePicker");
-var table = document.getElementById("pixelCanvas");
+const form = id("sizePicker");
+const table = id("pixelCanvas");
+
+// Creating function to get elements by id
+function id(id_name){
+  return document.getElementById(id_name);
+}
 
 // Creating a function to build a paint grid
 function makeGrid(event) {
   // Prevent form submission
   event.preventDefault();
+  // Clearing the grid
+  table.innerHTML = "";
   // Select size input values
-  var sizeH = document.getElementById("inputHeight").value;
-  var sizeW = document.getElementById("inputWidth").value;
+  let sizeH = id("inputHeight").value;
+  let sizeW = id("inputWidth").value;
   // Creating tr elements according to user's input
   for (i = 0; i < sizeH; i++) {
-    var tr = document.createElement('tr');
+    let tr = document.createElement('tr');
     // Creating td elements according to user's input
     for (k = 0; k< sizeW; k++) {
-      var td = document.createElement('td');
+      let td = document.createElement('td');
       // Appending td elements to tr elements
       tr.appendChild(td);
     }
@@ -22,10 +29,11 @@ function makeGrid(event) {
     table.appendChild(tr);
   }
 }
+
 // Creating a function to paint cells
 function paintCell(event) {
   // Select color input
-  var color = document.getElementById("colorPicker").value;
+  let color = id("colorPicker").value;
   // If a cell is not painted painting it with a selected color
   if (event.target.style.backgroundColor == "") {
     event.target.style.backgroundColor = color;
